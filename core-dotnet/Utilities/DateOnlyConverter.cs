def7024bcf95827d632952e0a3aa85704a23b858
omerfurkanwander <omerfurkan.wanderlust@gmail.com>
2023-03-25 15:12:53 +0300

    timeonly added to configuration

--- core-dotnet/Utilities/DateOnlyConverter.cs
+++ core-dotnet/Utilities/DateOnlyConverter.cs
@@ -4,8 +4,5 @@ namespace core_dotnet.Utilities;
 
 public class DateOnlyConverter : ValueConverter<DateOnly, DateTime>
 {
-    /// <summary>
-    /// Creates a new instance of this converter.
-    /// </summary>
     public DateOnlyConverter() : base(dateOnly => dateOnly.ToDateTime(TimeOnly.MinValue), dateTime => DateOnly.FromDateTime(dateTime)) { }
 }
