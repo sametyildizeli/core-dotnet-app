b9d5f907768c020f6fc74535402c6a436edd59b0
omerfurkanwander <omerfurkan.wanderlust@gmail.com>
2023-04-01 12:39:35 +0300

    Create user field nullable

--- core-dotnet/Entities/BaseEntity.cs
+++ core-dotnet/Entities/BaseEntity.cs
@@ -7,7 +7,7 @@ public class BaseEntity<TId> : IBaseEntity
     public TId Id { get; set; } = default!;
     public RecordStatus RecordStatus { get; set; } = RecordStatus.Active;
     public DateTimeOffset CreateDate { get; set; }
-    public string CreateUserId { get; set; } = default!;
+    public string? CreateUserId { get; set; }
     public DateTimeOffset UpdateDate { get; set; }
     public string? UpdateUserId { get; set; }
 }
--- core-dotnet/Entities/IBaseEntity.cs
+++ core-dotnet/Entities/IBaseEntity.cs
@@ -5,7 +5,7 @@ namespace core_dotnet.Entities
     public interface IBaseEntity
     {
         RecordStatus RecordStatus { get; set; }
-        string CreateUserId { get; set; }
+        string? CreateUserId { get; set; }
         DateTimeOffset CreateDate { get; set; }
         string? UpdateUserId { get; set; }
         DateTimeOffset UpdateDate { get; set; }
