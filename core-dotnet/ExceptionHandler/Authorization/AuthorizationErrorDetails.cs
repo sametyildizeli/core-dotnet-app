de1b401a10c345ff22849c495468f55e196d2825
omerfurkanwander <omerfurkan.wanderlust@gmail.com>
2023-04-07 21:08:58 +0300

    Error message carried to Errors field

--- core-dotnet/ExceptionHandler/Authorization/AuthorizationErrorDetails.cs
+++ core-dotnet/ExceptionHandler/Authorization/AuthorizationErrorDetails.cs
@@ -4,11 +4,15 @@ using Microsoft.AspNetCore.Mvc;
 namespace core_dotnet.ExceptionHandler.Authorization;
 public sealed class AuthorizationErrorDetails : ProblemDetails
 {
+    public object Errors { get; set; } = default!;
     public AuthorizationErrorDetails(string message)
     {
         Status = StatusCodes.Status401Unauthorized;
         Type = "Authorization Exception";
         Title = "Authorization Error";
-        Detail = message;
+        Errors = new List<string>
+        {
+            message
+        };
     }
 }
